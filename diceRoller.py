0698d6112982f9fba510c2028347517ac596ec38
Yamez <jcook7104@sohamcollege.org.uk>
2014-05-15 09:42:10 +0100

    Update diceRoller.py
    my changes to the dice roll programme.

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,8 @@
-#Coin flip program
+#Dice roll program
 #Describe the purpose of this program here.
 
-inport random;time
+import time
+import random;time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -11,25 +12,24 @@ s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+    print("rolling.....")
+    roll = rand.rand(int(0,7))
 
 
-def show_dice(roll)
-    if roll = 1:
+def show_dice(roll):
+    if roll == 1:
         print(S1)
-    else roll = 2
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll == 3:
         print(s3)
-    else roll = 4
+    elif roll == 4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    elif roll == 6:
         print(s6)
 
 roll
 time.sleep(1)
 show_dice(roll)
-
